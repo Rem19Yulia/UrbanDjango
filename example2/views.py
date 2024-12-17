213685dfcbee83a3854e4c3447d446957e243aa0
Rem19Yulia <kyv190591@gmail.com>
2024-12-17 16:41:06 +0300

    Update views.py

--- example2/views.py
+++ example2/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import TempLateView
 # Create your views here.
+
+def index(request):
+  return render(request, 'class_template.html')
+
+class index2(TempLateView):
+  template_name = "func_template.html"
