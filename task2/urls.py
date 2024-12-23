ff5d76d54436f40d8cef9f5bc6858addbab3c9c0
Rem19Yulia <kyv190591@gmail.com>
2024-12-23 23:37:43 +0300

    Update urls.py

--- task2/urls.py
+++ task2/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-   from .views import ClassView, function_view
+from UrbanDjango import views
 
-   urlpatterns = [
-       path('class/', ClassView.as_view(), name='class_view'),
-       path('function/', function_view, name='function_view'),
-   ]
+urlpatterns = [
+    path('class/', views.class),
+    path('func/', view.func), 
+]
